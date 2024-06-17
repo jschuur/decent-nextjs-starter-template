57c276cf4dbb6706e72bb770938cbb4d6c3152a3
Michele Memoli <michele.memoli@gmail.com>
2024-06-17 20:36:48 +0100

    Updated node fetch

--- src/lib/shared/services/db/db.ts
+++ src/lib/shared/services/db/db.ts
@@ -1,12 +1,15 @@
-import { createClient } from "@libsql/client/web";
-import { env } from "@shared/config/env";
-import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from '@libsql/client/web';
+import { env } from '@shared/config/env';
+import { drizzle } from 'drizzle-orm/libsql';
 
-import * as schema from "./schema";
+import * as schema from './schema';
 
 const client = createClient({
   url: env.DATABASE_URL,
   authToken: env.DATABASE_AUTH_TOKEN,
+  // use Node's default fetch, so we don't have to worry about cached queries from Next.js
+  // https://github.com/tursodatabase/libsql-client-ts/issues/38
+  fetch: global.fetch,
 });
 
 export const db = drizzle(client, {
