11995383fa9123762d69e603ca1fa478965e717c
Joost Schuur <jschuur@jschuur.com>
2024-07-11 19:05:15 +0100

    Framer motion hero header animation

--- src/components/Template/HeroHeader.tsx
+++ src/components/Template/HeroHeader.tsx
@@ -1,25 +1,48 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
 import { cn } from '@/lib/utils';
 
 type Props = {
   className?: string;
 };
 export default function HeroHeader({ className }: Props) {
+  // via https://variantvault.chrisabdo.dev/text-variants
+  const slideVariants = {
+    hidden: { opacity: 0, x: '-50vw' },
+    visible: { opacity: 1, x: 0 },
+    right: { opacity: 0, x: '50vw' },
+  };
+  const transitionDuration = 0.6;
+
   return (
-    <h1
-      className={cn(
-        'text-balance bg-gradient-to-br from-cyan-400 to-blue-700 bg-clip-text text-center font-sans text-5xl font-black leading-snug text-transparent md:text-[5rem] md:leading-tight',
-        className
-      )}
-    >
-      A Decent{' '}
-      <span className='underline decoration-amber-400 decoration-8 underline-offset-[12px]'>
-        Next.js
-      </span>
-      <br />{' '}
-      <span className='underline decoration-amber-400 decoration-8 underline-offset-[12px]'>
-        Starter
-      </span>{' '}
-      Template
-    </h1>
+    <div className={cn('overflow-hidden', className)}>
+      <motion.h1
+        initial='hidden'
+        animate='visible'
+        variants={slideVariants}
+        transition={{ duration: transitionDuration }}
+        className='bg-gradient-to-br from-cyan-400 to-blue-700 bg-clip-text text-center font-sans text-5xl font-black leading-snug text-transparent md:text-[5rem] md:leading-tight'
+      >
+        A Decent{' '}
+        <span className='underline decoration-amber-400 decoration-8 underline-offset-[12px]'>
+          Next.js
+        </span>
+      </motion.h1>
+
+      <motion.h1
+        initial='right'
+        animate='visible'
+        variants={slideVariants}
+        transition={{ duration: transitionDuration }}
+        className='bg-gradient-to-br from-cyan-400 to-blue-700 bg-clip-text text-center font-sans text-5xl font-black leading-snug text-transparent md:text-[5rem] md:leading-tight'
+      >
+        <span className='underline decoration-amber-400 decoration-8 underline-offset-[12px]'>
+          Starter
+        </span>{' '}
+        Template
+      </motion.h1>
+    </div>
   );
 }
